118447498328240e2e68758ac1a201149ff5fa3d
vayvatechnologies <vayvatechnologies@gmail.com>
2026-03-19 23:40:57 +0530

    Update db.js

--- PravBackendServer/utils/db.js
+++ PravBackendServer/utils/db.js
@@ -18,12 +18,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-   connectionString: process.env.DATABASE_URL,
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: 5432,
+   connectionString: process.env.DATABASE_URL2,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASS,
+  // port: 5432,
   ssl: {
     rejectUnauthorized: false
   }
