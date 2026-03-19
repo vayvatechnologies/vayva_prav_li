a2ba7e3591c97c9a28ba52ce92546a2fd208f385
tppraveen <praveentp2001@gmail.com>
2026-03-19 23:47:21 +0530

    Final DB Connection Integrated with Supabase DB

--- PravBackendServer/utils/db.js
+++ PravBackendServer/utils/db.js
@@ -1,29 +1,8 @@
-// // db.js
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASS,
-//   port: process.env.DB_PORT,
-// });
-
-// module.exports = pool;
-
-
-// db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-   connectionString: process.env.DATABASE_URL,
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
